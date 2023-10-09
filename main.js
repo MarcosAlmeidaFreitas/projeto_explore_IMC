981477f41dd229daf409b49e0be33633c42070c8
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2023-10-09 15:28:09 -0300

    animation

--- main.js
+++ main.js
@@ -1,26 +1,46 @@
+const form = document.querySelector('form');
 const input_weight = document.querySelector('#input_weight');
 const input_height = document.querySelector('#input_height');
 const btn_calc = document.querySelector('#btn_calc');
 const btn_close = document.querySelector('#btn_close')
 
-btn_calc.addEventListener("click", tryClickSubmit);
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modalMessage = document.querySelector('.modal .title span')
 
-function tryClickSubmit(event){
+form.onsubmit = function(event){
   event.preventDefault();
-  let weight = input_weight.value;
-  let height = input_height.value;
 
-  let IMC = weight / (height * height);
-
-  if(IMC < 17){
-
-  }
+  const weigth = input_height.value
+  const heigth = input_height.value
+  const imc = calcular_IMC(weigth, heigth);
 }
+
+
 
 function toggleScreen(){
   
 }
 
+function calcular_IMC(weigth, heigth){
+  return (weigth / ((heigth / 100) ** 2)).toFixed(2);
+}
 
+function IMC(imc){
+  if(imc < 17){
+    return 'Muito abaixo do peso'
+  }else if(imc >= 17 && imc <= 18.49){
+    return 'Abaixo do peso';
+  }else if(imc>=18.5 && imc<=24.99){
+    return 'Peso normal'
+  }else if(imc>= 25 && imc<=29.99){
+    return 'Acima do peso'
+  }else if(imc>=30 && imc<=34.99){
+    return 'Obesidade grau 1'
+  }else if(imc >= 35 && imc<=39.99){
+    return 'Obesidade grau 2 (severa)'
+  }else if(imc >= 40){
+    return 'Obesidade grau 3 (obesidade mórbida)'
+  }
+}
 
 
